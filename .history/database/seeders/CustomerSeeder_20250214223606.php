91925ace4b15c9ea6ef7874e0857b64e9874dd23
haakim19 <haakimahamed4@gmail.com>
2025-02-14 23:24:01 +0530

    history update

---
+++ .history/database/seeders/CustomerSeeder_20250214223606.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Pest\ArchPresets\Custom;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Customer::factory()
+        ->
+    }
+}
---
+++ .history/database/seeders/CustomerSeeder_20250214223733.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Pest\ArchPresets\Custom;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Customer::factory()
+            ->count(25)
+            ->hasInvoices(10)
+            ->create();
+
+        Customer::factory()
+            ->count(25)
+            ->hasInvoices(10)
+            ->create();
+
+        Customer::factory()
+            ->count(25)
+            ->hasInvoices(10)
+            ->create();
+
+        Customer::factory()
+            ->count(25)
+            ->hasInvoices(10)
+            ->create();
+
+        Customer::factory()
+            ->count(25)
+            ->hasInvoices(10)
+            ->create();
+    }
+}
