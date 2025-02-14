169cf801226cb84a736788fff73efa8b88110f83
haakim19 <haakimahamed4@gmail.com>
2025-02-14 23:24:01 +0530

    history update

---
+++ .history/database/seeders/CustomerSeeder_20250214223555.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Pest\ArchPresets\Custom;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Customer::factory
+    }
+}
---
+++ .history/routes/api_20250214230840.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\api\V1\CustomerController;
+use App\Http\Controllers\api\V1\InvoiceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Models\Controllers\api\V1'], function () {
+    Route::apiResource('/customers', CustomerController::class);
+    Route::apiResource('/invoice', InvoiceController::class);
+});
