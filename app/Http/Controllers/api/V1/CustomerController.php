8b200562f84fd3be5d1cac0f7d99882d70924372
haakim19 <haakimahamed4@gmail.com>
2025-02-15 13:32:05 +0530

    Data Filtering

--- app/Http/Controllers/api/V1/CustomerController.php
+++ app/Http/Controllers/api/V1/CustomerController.php
@@ -8,15 +8,23 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
+use Illuminate\Http\Request;
+use App\Services\V1\CustomerQuery;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new CustomerCollection(Customer::paginate(5));
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request);
+        if (count($queryItems) == 0) {
+            return new CustomerCollection(Customer::paginate());
+        } else {
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
     }
 
     /**
