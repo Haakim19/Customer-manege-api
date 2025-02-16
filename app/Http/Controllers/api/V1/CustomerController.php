725ef3f659e1cbf797ef265c6c742917bd108074
haakim19 <haakimahamed4@gmail.com>
2025-02-16 23:09:48 +0530

    Updating data with put and patch

--- app/Http/Controllers/api/V1/CustomerController.php
+++ app/Http/Controllers/api/V1/CustomerController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\api\V1;
 
 use App\Models\Customer;
-use App\Http\Requests\UpdateCustomerRequest;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Filters\V1\CustomerFilter;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
-use Illuminate\Http\Request;
-use App\Filters\V1\CustomerFilter;
 use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 
 class CustomerController extends Controller
 {
@@ -33,25 +33,11 @@ class CustomerController extends Controller
         );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreCustomerRequest $request)
     {
         return new CustomerResource(Customer::create($request->all()));
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Customer $customer)
     {
         $includeInvoices = request()->query('includeInvoices');
@@ -66,7 +52,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
